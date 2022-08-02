e18a0db1bf6f536c92b51115bd99b235c72b9e22
imamyy <amytran2608@gmail.com>
2022-08-02 14:56:01 -0400

    Added script.js file for test

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const btn = document.getElementById('btn')
-
-const addText = () =>{
-    document.body.append('It seems as if it has been clicked!')
-}
-
-const changeBackground = () => {
-    document.body.classList.toggle('black')
-}
-
-btn.addEventListener('click', changeBackground)
+const btn = document.getElementById("btn");
+console.log(btn);
+btn.addEventListener("click", () => {
+  console.log("Element clicked through function!");
+});
+// Add text to the page on click
+btn.addEventListener("click", () => {
+  document.body.append("It seems as if it has been clicked!");
+});
+// Use a named, referenced function as the click handler for the listener
+// Separate named function addText
+const addText = () => {
+  document.body.append("It seems as if it has been clicked!");
+};
+// Then reference this in the event Listener
+btn.addEventListener("click", addText);
+// Toggle the background-color of the page
+const changeClass = () => {
+  document.body.classList.toggle("black");
+};
+btn.addEventListener("click", changeClass);
